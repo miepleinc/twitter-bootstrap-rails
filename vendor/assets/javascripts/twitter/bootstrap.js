2afef6c7c144f8253b1712a660dc1824f65021d4
Tim Jones <tim@timjones.tw>
2012-01-23 09:46:17 +0800

    Fixed LESS parsing issue

--- vendor/assets/javascripts/twitter/bootstrap.js
+++ vendor/assets/javascripts/twitter/bootstrap.js
@@ -1,10 +1,10 @@
 //= require twitter/bootstrap/bootstrap-alert
 //= require twitter/bootstrap/bootstrap-dropdown
 //= require twitter/bootstrap/bootstrap-modal
+//= require twitter/bootstrap/bootstrap-tooltip
 //= require twitter/bootstrap/bootstrap-popover
 //= require twitter/bootstrap/bootstrap-scrollspy
 //= require twitter/bootstrap/bootstrap-tab
-//= require twitter/bootstrap/bootstrap-tooltip
 //= require twitter/bootstrap/bootstrap-transition
 //= require twitter/bootstrap/bootstrap-typeahead
 //= require twitter/bootstrap/bootstrap-button
